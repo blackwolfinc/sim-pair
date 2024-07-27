ba6c286de8d58ca6e761bbadb3361fcef32b8865
celotip <lino.marcel148@gmail.com>
2024-07-27 09:37:36 +0700

    added comment

--- aplikasi/main.go
+++ aplikasi/main.go
@@ -2,4 +2,6 @@ package main
 
 func main() {
 	// tesss
+	// 1. Tambah Produk Memungkinkan pengguna untuk menambah produk baru ke dalam database. 
+	// Pengguna akan diminta untuk memasukkan nama produk, harga, dan jumlah stok awal.
 }
